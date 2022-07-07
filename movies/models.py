05d4e8c2ff619817d008d8c3ebe8e70f2c883321
DenChist <79021897+DenChist@users.noreply.github.com>
2022-07-07 23:27:31 +0300

    update

--- movies/models.py
+++ movies/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Movie(models.Model):
     movie_id = models.IntegerField(null=False)
     name = models.CharField(max_length=250)
+    genre = models.TextField(max_length=250)
+    year = models.IntegerField(null=False)
+    country = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    age_rating = models.CharField(max_length=16)
     description = models.TextField(max_length=5000)
     image = models.ImageField(upload_to='movie')
-    torrent_id = models.CharField(max_length=4096)
+    torrent = models.FileField(upload_to='torrent', null=True)
 
     class Meta:
         verbose_name = 'фильм'
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Movie
-
+import bencodepy
+import hashlib
+import base64
 
 def index_page(request):
     movies = Movie.objects.all()
-    return render(request, 'movies/index.html', {'movies': movies})
+    return render(request, 'main.html', {'movies': movies})
 
 
 def player_page(request, movie_id):
     movie = Movie.objects.get(movie_id=movie_id)
-    return render(request, 'movies/player.html', {'movie': movie})
+
+    magnet = ""
+    return render(request, 'season.html', {'movie': movie, 'magnet': magnet})
