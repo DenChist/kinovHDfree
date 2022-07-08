5afa8f998be98415652bbb1427c3e654cd4df491
DenChist <79021897+DenChist@users.noreply.github.com>
2022-07-08 23:54:36 +0300

    update

--- movies/views.py
+++ movies/views.py
@@ -11,6 +11,14 @@ def index_page(request):
 
 def player_page(request, movie_id):
     movie = Movie.objects.get(movie_id=movie_id)
-
-    magnet = ""
+    metadata = bencodepy.decode_from_file(movie.torrent.path)
+    subj = metadata[b'info']
+    hashcontents = bencodepy.encode(subj)
+    digest = hashlib.sha1(hashcontents).digest()
+    b32hash = base64.b32encode(digest).decode()
+    magnet = 'magnet:?'\
+             + 'xt=urn:btih:' + b32hash\
+             + '&dn=' + metadata[b'info'][b'name'].decode()\
+             + '&tr=' + metadata[b'announce'].decode() + '&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'\
+             + '&xl=' + str(metadata[b'info'][b'length'])
     return render(request, 'season.html', {'movie': movie, 'magnet': magnet})
